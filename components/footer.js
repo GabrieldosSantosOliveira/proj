694b2aca08a18b829a29984637d5e227e2d823c6
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-04-27 10:21:16 -0400

    feito melhorias

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+export default function footer() {
+    return(
+        <>
+        <div className='flex align-center justify-center'>
+        2022 - © PROMATCH - TODOS OS DIREITOS RESERVADOS.
+        </div>
+        </>
+    )
+}
